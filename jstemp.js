ae64dbf9de4b32bd972e6a7b9f8519232766ea8c
ArsenicChaff <163602000+ArsenicChaff@users.noreply.github.com>
2024-04-02 11:46:17 -0400

    Missed edits to be saved on last commit

--- jstemp.js
+++ jstemp.js
@@ -10,9 +10,6 @@
 
 let input = parseInt(prompt("enter an integer above 1"))
 
-
-//counter
-//for as long as; temp<inputInt, run below code; increase temp by 1
 for (let temp = 1; temp <= input; temp++) {
     // console.log(temp);
     if ((temp % 3 === 0) && (temp % 5 === 0)) {
@@ -25,29 +22,3 @@ for (let temp = 1; temp <= input; temp++) {
         console.log(temp)
     }
 }
-
-
-//overall
-// function overall(input) {
-//     if checkFizzBuzz(input) {
-//         return checkFizzBuzz();
-//     } else return input;
-// }
-
-
-
-//checking multiple of three and five
-function checkFizzBuzz(input) {
-    if (((input % 3) === 0) && ((input % 5) === 0)) {
-        let output = "FizzBuzz"
-        return output;
-    } else if ((input % 3) === 0) {
-        let fizz = "Fizz";
-        return fizz;
-    } else if ((input % 5) === 0) {
-        let buzz = "Buzz";
-         return buzz;
-    }
-}
-
-// console.log(checkFizzBuzz(inputInt))
